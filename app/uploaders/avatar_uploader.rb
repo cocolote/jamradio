5e03a1cfb9424a03a215760a65cfe7514062b0cf
Ezequiel Jose Lopez <skiel.j.lopez@gmail.com>
2015-04-25 23:01:40 -0400

    added default picture

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,6 +3,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  end
+
   if Rails.env.production? || Rails.env.development?
     storage :fog
   else
